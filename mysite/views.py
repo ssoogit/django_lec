48859c692387d474d50b10a035278fdbb933142d
ssoogit <ssoogit02@gmail.com>
2023-11-13 04:35:39 +0900

    2주차 django 개발 시작

---
+++ mysite/views.py
@@ -0,0 +1,12 @@
+# from django.http import HttpResponse
+
+from django.shortcuts import render
+from .models import MainContent
+
+def index(request):
+    # return HttpResponse("Hello world")
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_list.html', context)
+
+# Create your views here.
